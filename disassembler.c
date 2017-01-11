ef865a0ae251b1a0cc8610b9f3cb5b2c6cbec530
IsThisTheFuture <mail@tobiaslamm.de>
2017-01-11 16:08:14 +0100

    Not much changed

--- disassembler.c
+++ disassembler.c
@@ -9,10 +9,11 @@ int main (void) {
 }
 
 /*
- * codebuffer is a valid pointer to 8080 assembly code
- * pc is the current offset into the code
+ * Gibt die Anzahl Bytes der Operation zurück (Im Regelfall 1)
  *
- * returns the number of bytes of the op
+ * codebuffer ist ein Zeiger auf 8080 Assembler-Code
+ * pc ist das aktuelle Offset im Code
+ *
  */
 
 int Disassemble8080 (unsigned char *codebuffer, int pc)
@@ -46,8 +47,7 @@ int Disassemble8080 (unsigned char *codebuffer, int pc)
 	case 0x12: printf("STAX   D"); break;      
 	case 0x13: printf("INX    D"); break;
 	case 0x14: printf("INR    D"); break;
-        case 0x15: printf("DCR    D"); break; 
-	case 0x16: printf("MVI    D,#$%02x", code[1]); opbytes=2; break;      
+        case 0x15: printf("DCR    D"); break$%02x", code[1]); opbytes=2; break;      
 	case 0x17: printf("RAL"); break;
 	case 0x18: printf("NOP"); break;
 	case 0x19: printf("DAD    D"); break; 
@@ -58,9 +58,35 @@ int Disassemble8080 (unsigned char *codebuffer, int pc)
 	case 0x1e: printf("MVI    E,#$%02x", code[1]); opbytes=2; break;      
 	case 0x1f: printf("RAR"); break;
 	case 0x20: printf("RIM"); break;
-	
-	
-		   /* ........ */
+	case 0x21: printf(""); break;
+	case 0x22: printf(""); break;
+	case 0x23: printf(""); break;	 
+	case 0x24: printf(""); break; 
+	case 0x25: printf(""); break; 
+	case 0x26: printf(""); break;
+	case 0x27: printf(""); break; 
+	case 0x28: printf(""); break;
+	case 0x29: printf(""); break;
+	case 0x2a: printf(""); break;
+	case 0x2b: printf(""); break;
+	case 0x2c: printf(""); break;
+	case 0x2d: printf(""); break;
+	case 0x2e: printf(""); break;
+	case 0x2f: printf(""); break;
+	case 0x30: printf(""); break;
+	case 0x31: printf(""); break;
+	case 0x32: printf(""); break;
+	case 0x33: printf(""); break;
+	case 0x34: printf(""); break;
+	case 0x35: printf(""); break;
+	case 0x36: printf(""); break;
+	case 0x37: printf(""); break;
+	case 0x38: printf(""); break;
+	case 0x39: printf(""); break;
+	case 0x3a: printf(""); break;
+	case 0x3b: printf(""); break;
+	case 0x3c: printf(""); break;
+	case 0x3d: printf(""); break;
         case 0x3e: printf("MVI    A,#0x%02x", code[1]); opbytes = 2; break;
             /* ........ */
         case 0xc3: printf("JMP    $%02x%02x",code[2],code[1]); opbytes = 3; break;
